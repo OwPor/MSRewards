087acfeb66cd80ef056fea97ec8b267f6ba24112
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2024-04-30 00:00:59 +0800

    Create main.py

---
+++ Tkinter/main.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+from tkinter import messagebox as mb
+from tkinter.font import Font
+import pyautogui as pyag
+import keyboard as kb
+from pynput import keyboard, mouse
+from wonderwords import RandomWord
+import time
+
+urlx = 0
+urly = 0
+total = 0
+scheduled_event = None
+
+def urlPos(key):
+    global urlx, urly
+
+    try:
+        if key.char == 'k':
+            urlx, urly = mouse.Controller().position
+            return False
+    except AttributeError:
+        pass
+
+def getUrlPos():
+    listener = keyboard.Listener(on_press=urlPos)
+    listener.start()
+    listener.join()
+    mb.showinfo('URL Position', 'X: ' + str(urlx) + ', Y: ' + str(urly))
+
+def search(times):
+    def perform_action():
+        global running, scheduled_event
+        nonlocal i
+        
+        rwg = RandomWord()
+        pyag.click(urlx, urly)
+        kb.write(rwg.random_words())
+        kb.write(' ')
+        kb.press_and_release('Backspace')
+        kb.press_and_release('Enter')
+        i += 1
+        global total
+        total = i
+        if not running or i >= times:
+            if i >= times:
+                root.after(1000)
+                mb.showinfo('Done!', 'Times: ' + str(times))
+            return
+        scheduled_event = root.after(7000, perform_action)
+    i = 0
+    perform_action()
+
+def start():
+    global running, counts
+    running = True
+    counts = sp.get()
+    if int(counts) == 0:
+        mb.showinfo('Times', 'Enter how many times first')
+        return
+    
+    if urlx == 0 and urly == 0:
+        mb.showinfo('Set Position', 'Set positions first')
+        return
+    
+    bool(search(int(counts)))
+
+def cancel():
+    global running, scheduled_event
+    running = False
+    if scheduled_event is not None:
+        root.after_cancel(scheduled_event)
+    mb.showinfo('Done!', 'Times: ' + str(total))
+
+root = tk.Tk()
+root.resizable(False, False)
+root.geometry('300x270')
+root.iconbitmap('icon.ico')
+root.title('Auto Search by OwPor')
+
+label = tk.Label(root, text='How many times?:', font=('Times New Roman', 12))
+label.pack(padx=10, pady=8)
+sp = tk.Spinbox(root, width=5, from_=0, to=100, wrap=True, font=Font(family='Helvetica', size=14))
+sp.pack()
+
+label = tk.Label(root, text='Press K when button is clicked to set position', font=('Times New Roman', 12))
+label.pack(padx=10, pady=8)
+
+url_btn = tk.Button(root, text='Set URL Pos', command=getUrlPos, font=('Times New Roman', 12))
+url_btn.pack(padx=10, pady=8)
+
+start_btn = tk.Button(root, text='Start', command=start, font=('Times New Roman', 12))
+start_btn.pack(padx=10, pady=8)
+
+cancel_btn = tk.Button(root, text='Cancel', command=cancel, font=('Times New Roman', 12))
+cancel_btn.pack(padx=10, pady=8)
+
+root.mainloop()
